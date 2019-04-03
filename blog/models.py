5d9258eeb7fa7d287fa1dc25fae4ddeaf4290176
bhanu <dubeybhanuja44@gmail.com>
2019-04-03 06:26:24 -0800

    Changed the HTML site.

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
@@ -17,5 +16,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
-# Create your models here.
--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f6edj-=ajlk4_@r17(yke#9cfq%uxr&qgxskz!70f52r)jr!y&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG =True
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
